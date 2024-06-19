8560f94a7f1706e5b25d884eb4c1351036aaa036
Zander Lewis <wolfthedev@gmail.com>
2024-06-19 15:56:35 -0400

    Initial Upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let folders = [
+    { name: "Mini Colonies", path: "mini-colonies" },
+    { name: "Endless Waves", path: "endless-waves" },
+];
+
+// Function to list folders
+function listFolders(folders) {
+    let folderList = document.getElementById('folderList');
+
+    folders.forEach(folder => {
+        let listItem = document.createElement('li');
+        let link = document.createElement('a');
+        link.href = folder.path;
+        link.textContent = folder.name;
+        listItem.appendChild(link);
+        folderList.appendChild(listItem);
+    });
+}
+
+// Call the function with your folders
+listFolders(folders);
